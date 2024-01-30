601f78c3087dd4a1479c2ff58f323842c4b14ce6
Kumar-SShekhar <kumar.shekhar@indusnet.co.in>
2024-01-30 18:46:38 +0530

    Commit

--- SimpleSpringSecurityDemo/src/main/java/com/kss/SimpleSpringSecurityDemo/config/AppSecurityConfig.java
+++ SimpleSpringSecurityDemo/src/main/java/com/kss/SimpleSpringSecurityDemo/config/AppSecurityConfig.java
@@ -5,31 +5,66 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class AppSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/public").permitAll()
-                        .anyRequest().authenticated())
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails abc = User.builder()
+                .username("abc")
+                .password(passwordEncoder().encode("abc"))
+                .roles("USER")
+                .build();
+
+        UserDetails xyz = User.builder()
+                .username("xyz")
+                .password(passwordEncoder().encode("xyz"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(abc, xyz);
     }
 
 //    @Bean
-//    public
+//    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/public/*").permitAll()
+//                        .anyRequest().authenticated())
+//                .httpBasic(Customizer.withDefaults());
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
 
 }
--- SimpleSpringSecurityDemo/src/main/java/com/kss/SimpleSpringSecurityDemo/controller/UserController.java
+++ SimpleSpringSecurityDemo/src/main/java/com/kss/SimpleSpringSecurityDemo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.kss.SimpleSpringSecurityDemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class UserController {
         return "Admin";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/public/abc")
     public String getAllPublicAbc(){
         return "Admin abc";
